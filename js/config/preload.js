acb96b69880077c0941c2e20781ed9cbd6afd49f
lelode <lys6370@gmail.com>
2016-03-23 22:40:31 +0900

    undertale

---
+++ js/config/preload.js
@@ -0,0 +1,24 @@
+var introImages = new Array();
+
+function initImages () {
+	introImages[1] = new Image();
+	introImages[1].src = "img/intro/1.png";
+	introImages[2] = new Image();
+	introImages[2].src = "img/intro/2.png";
+	introImages[3] = new Image();
+	introImages[3].src = "img/intro/3.png";
+	introImages[4] = new Image();
+	introImages[4].src = "img/intro/4.png";
+	introImages[5] = new Image();
+	introImages[5].src = "img/intro/5.png";
+	introImages[6] = new Image();
+	introImages[6].src = "img/intro/6.png";
+	introImages[7] = new Image();
+	introImages[7].src = "img/intro/7.png";
+	introImages[8] = new Image();
+	introImages[8].src = "img/intro/8.png";
+	introImages[9] = new Image();
+	introImages[9].src = "img/intro/9.png";
+	introImages[10] = new Image();
+	introImages[10].src = "img/intro/10.png";
+}
---
+++ js/main.js
@@ -0,0 +1,149 @@
+var introImage, ctxImage, ctxScript;
+var canvas = document.getElementById("game_container");
+var introIdx = 0;
+
+var introImages = new Array();
+
+var testString;
+var bgmIntro;
+
+function initBGM(){
+	bgmIntro = new Audio();
+	bgmIntro.src = "sounds/BGM/01_OnceUponATime.mp3"
+	bgmIntro.loop = true;
+	bgmIntro.play();
+}
+
+function initImages () {
+	introImages[1] = new Image();
+	introImages[1].src = "img/intro/1.png";
+	introImages[2] = new Image();
+	introImages[2].src = "img/intro/2.png";
+	introImages[3] = new Image();
+	introImages[3].src = "img/intro/3.png";
+	introImages[4] = new Image();
+	introImages[4].src = "img/intro/4.png";
+	introImages[5] = new Image();
+	introImages[5].src = "img/intro/5.png";
+	introImages[6] = new Image();
+	introImages[6].src = "img/intro/6.png";
+	introImages[7] = new Image();
+	introImages[7].src = "img/intro/7.png";
+	introImages[8] = new Image();
+	introImages[8].src = "img/intro/8.png";
+	introImages[9] = new Image();
+	introImages[9].src = "img/intro/9.png";
+	introImages[10] = new Image();
+	introImages[10].src = "img/intro/10.png";
+}
+
+canvas.addEventListener("click", processIntro);
+window.addEventListener("load", initImages);
+
+function processIntro() {
+	ctxScript.fillStyle = "black";
+	ctxScript.fillRect(0, 0, 470, 110);
+	ctxImage.fillStyle = "black";
+	ctxImage.fillRect(0, 0, 398, 214);
+	introIdx++;
+
+	showIntroImg();
+	showIntroScript();
+}
+
+window.onload= function(){
+	Intro();
+}
+
+function Intro() {
+	introImage = document.createElement("canvas");
+	introScriptCanvas = document.createElement("canvas");
+
+	introImage.width = 398;
+	introImage.height = 214;
+	introScriptCanvas.width = 470;
+	introScriptCanvas.height = 110;
+
+	ctxImage = introImage.getContext("2d");
+	ctxScript = introScriptCanvas.getContext("2d");
+	document.getElementById("game_container").appendChild(introImage);
+	document.getElementById("game_container").appendChild(introScriptCanvas);
+
+	introImage.style.position = "absolute";
+	introImage.style.top = "65px";
+	introImage.style.left = "115px";
+
+	introScriptCanvas.style.position = "absolute";
+	introScriptCanvas.style.top = "325px";
+	introScriptCanvas.style.left = "115px";
+
+	ctxScript.fillStyle = "white";
+	ctxScript.font = "25px Arial";
+
+	var loop = function() {
+		draw();
+	}
+
+	showIntroImg();
+}
+
+function showIntroScript(){
+	ctxScript.fillStyle = "white";
+
+	switch (introIdx){
+		case 1:
+			ctxScript.fillText("먼 옛날, ", 0, 20);
+			ctxScript.fillText("두 종족이 지구를 지배하고 있었다.", 0, 60);
+			ctxScript.fillText("바로 인간과 괴물이었다.", 0, 100);
+			break;
+		case 2:
+			ctxScript.fillText("어느 날, 두 종족 사이에 전쟁이", 0, 20);
+			ctxScript.fillText("벌어졌다.", 0, 60);
+			break;
+		case 3:
+			ctxScript.fillText("기나긴 싸움 끝에, 인간들은", 0, 20);
+			ctxScript.fillText("승기를 잡았다.", 0, 60);
+			break;
+		case 4:
+			ctxScript.fillText("인간들은 마법 주문을 사용하여", 0, 20);
+			ctxScript.fillText("괴물들을 지하세계에 봉인하였다.", 0, 60);
+			break;
+		case 5:
+			ctxScript.fillText("그 후로 오랜 시간이 흘렀다...", 0, 20);
+			break;
+		case 6:
+			ctxScript.fillText("에봇 산", 170, 20);
+			ctxScript.fillText("201X년", 170, 60);
+			break;
+		case 7:
+			ctxScript.fillText("전설에 의하면, 에봇 산에 한번 오르면", 0, 20);
+			ctxScript.fillText("다시는 돌아오지 못한다고 한다.", 0, 60);
+			break;
+		default: break;
+	}
+	
+}
+
+function showIntroImg(){
+	introImage.style.opacity = 0;
+	ctxImage.drawImage(introImages[introIdx], 0, 0);
+	unfade(introImage);
+}
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+function typewrite() {
+
+}
+
